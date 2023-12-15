a161dc9f28657f6382a5a845c52b27e15a2b9fb7
rashadmohamedr <51525838+rashadmohamedr@users.noreply.github.com>
2023-12-15 14:40:38 +0200

    Update Bricks.cpp

--- Bricks.cpp
+++ Bricks.cpp
@@ -11,7 +11,7 @@ brick::brick(point r_uprleft, int r_width, int r_height, game* r_pGame):
 normalBrick::normalBrick(point r_uprleft, int r_width, int r_height, game* r_pGame):
 	brick(r_uprleft, r_width, r_height, r_pGame)
 {
-	imageName = "images\\bricks\\NormalBrick.jpg";
+	imageName = "images\\bricks\\NormalBrick.jpg",strength=1;
 }
 void normalBrick::collisionAction()
 {
@@ -20,11 +20,17 @@ void normalBrick::collisionAction()
 hardBrick::hardBrick(point r_uprleft, int r_width, int r_height, game* r_pGame):
 	brick(r_uprleft, r_width, r_height, r_pGame)
 {
-	imageName = "images\\bricks\\HardBrick.jpg";
+	imageName = "images\\bricks\\HardBrick.jpg",strength=3;
 }
 
 void hardBrick::collisionAction()
 {
+	//TODO: Add collision action logic
+	strength--;
+	//score++;
+	if (!strength) {
+		//delete brick
+	}
 }
 
 
